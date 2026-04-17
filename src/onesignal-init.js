6aefebe02bc06695406c776866c457d1a0914e05
Samuele Montagner <samuele.montagner2009@gmail.com>
2026-04-17 16:16:57 +0200

    aggiornamento

--- src/onesignal-init.js
+++ src/onesignal-init.js
@@ -6,8 +6,8 @@ export async function initOneSignal() {
     appId: '934b6cd9-e8c6-468e-8d3f-c1133c347092',
     notifyButton: { enable: true },
     allowLocalhostAsSecureOrigin: true,
-    serviceWorkerPath: '/OneSignalSDKWorker.js',
-    serviceWorkerUpdaterPath: '/OneSignalSDKUpdaterWorker.js',
+    serviceWorkerPath: '/OneSignalSDKWorker2.js',
+    serviceWorkerUpdaterPath: '/OneSignalSDKUpdaterWorker2.js',
   });
   // Mostra il prompt per la richiesta notifiche solo se la funzione esiste
   if (typeof OneSignal.showSlidedownPrompt === 'function') {
