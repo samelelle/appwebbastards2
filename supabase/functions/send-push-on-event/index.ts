ea82799ed23923c212fc99bf789fe8bd25884ecb
Samuele Montagner <samuele.montagner2009@gmail.com>
2026-04-18 18:20:38 +0200

    aggiornamento

--- supabase/functions/send-push-on-event/index.ts
+++ supabase/functions/send-push-on-event/index.ts
@@ -4,7 +4,17 @@ const SUPABASE_URL = 'https://yqapipzwmgvuzduqnsps.supabase.co';
 const SUPABASE_SERVICE_ROLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxYXBpcHp3bWd2dXpkdXFuc3BzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTczOTQ5OCwiZXhwIjoyMDkxMzE1NDk4fQ.maFqfRJQr0uD8DMCqHDQT9DDljrI53TPAdzEcK51eZA';
 
 Deno.serve(async (req) => {
-  const { authorId, title, body, url } = await req.json();
+  // Gestione preflight CORS
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { status: 200 });
+  }
+  let dataJson = {};
+  try {
+    dataJson = await req.json();
+  } catch {
+    return new Response("Invalid JSON", { status: 400 });
+  }
+  const { authorId, title, body, url } = dataJson;
   const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
   const { data } = await supabase.from('push_subscriptions').select('*').neq('user_id', authorId);
   if (data) {
