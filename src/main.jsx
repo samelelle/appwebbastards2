16858ee383ced73f499e88f87b5572f216dfe081
Samuele Montagner <samuele.montagner2009@gmail.com>
2026-05-04 18:10:57 +0200

    aggiornamento

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/push-sw.js').catch(() => {});
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
