9cbe1c24e266c2edb96437ef2f2b111eea1372f8
Samuele Montagner <samuele.montagner2009@gmail.com>
2026-05-06 20:58:55 +0200

    aggiornamento

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,30 @@ import App from './App.jsx';
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     fetch('/sw-manifest.json')
-      .then(res => res.json())
+      .then(res => {
+        console.log('[SW DEBUG] sw-manifest.json status:', res.status);
+        console.log('[SW DEBUG] sw-manifest.json content-type:', res.headers.get('content-type'));
+        if (!res.ok) throw new Error('sw-manifest.json request failed');
+        return res.json();
+      })
       .then(({ sw }) => {
-        navigator.serviceWorker.register(`/${sw}`).catch(() => {});
+        const swUrl = `/${sw}`;
+        console.log('[SW DEBUG] registering service worker:', swUrl);
+        navigator.serviceWorker.register(swUrl)
+          .then(reg => {
+            console.log('[SW DEBUG] service worker registered, scope:', reg.scope);
+          })
+          .catch(err => {
+            console.log('[SW DEBUG] service worker register error:', err);
+          });
+        fetch(swUrl, { cache: 'no-store' })
+          .then(res => {
+            console.log('[SW DEBUG] sw script status:', res.status);
+            console.log('[SW DEBUG] sw script content-type:', res.headers.get('content-type'));
+          })
+          .catch(err => {
+            console.log('[SW DEBUG] sw script fetch error:', err);
+          });
       });
   });
 }
