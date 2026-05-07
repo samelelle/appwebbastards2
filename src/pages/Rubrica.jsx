8a5fa8dec0b4cefee6ae13ccb0395d015a061b9d
Samuele Montagner <samuele.montagner2009@gmail.com>
2026-05-07 21:45:05 +0200

    aggiornamento

--- src/pages/Rubrica.jsx
+++ src/pages/Rubrica.jsx
@@ -152,6 +152,7 @@ function Rubrica({ isDevMode, maintenanceMode }) {
   const lastSeenMsgIdRef = useRef({});
   const knownMessageIdsRef = useRef(new Set());
   const initializedMessagesRef = useRef(null);
+  const chatEndRef = useRef(null);
   const [chatPermissionError, setChatPermissionError] = useState('');
   const [openedChatImage, setOpenedChatImage] = useState('');
   const [notificationsAllowed, setNotificationsAllowed] = useState(false);
